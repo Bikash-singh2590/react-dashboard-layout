a768adbf26c1dd5e26e5abb9b5529ef38513d59f
Bikash-singh <bikash.singh@botmantra.com>
2021-01-21 18:30:20 +0530

    completed

--- src/App.js
+++ src/App.js
@@ -2,7 +2,8 @@ import React, { Component } from 'react';
 import { Route, Switch } from 'react-router-dom';
 import login from '../src/components/login/login'
 import Layout from './container/Layout/Layout';
-import dashboard from  './container/Dashboard/Dashboard'
+import dashboard from  './container/Dashboard/Dashboard';
+import about from './container/About/About'
 class App extends Component {
   render() {
     return (
@@ -12,8 +13,8 @@ class App extends Component {
           <Route path="/login" exact component={login} />
           <Layout>
             <Route path="/Dashboard" component={dashboard}  />
-            <Route path="/Charts" />
-            <Route path="/About" />
+            <Route path="/Charts" component={about}/>
+            <Route path="/About" component={about} />
           </Layout>
 
         </Switch>
---
+++ src/container/About/About.js
@@ -0,0 +1,93 @@
+import React, { Component } from 'react'
+import classes from '../../container/Dashboard/Dashboard.module.css';
+import Paper from '@material-ui/core/Paper';
+import Grid from '@material-ui/core/Grid';
+import Typography from '@material-ui/core/Typography';
+
+class Dashboard extends Component {
+    render() {
+        return (
+            <React.Fragment>
+                {/* ---Ageing Report Summary---*/}
+                <div className="Ageing Report Summary">
+                    <div>
+                        <Grid container spacing={3}>
+                            <Grid item xs={12}>
+                                <Paper className={classes.Paper} style={{ background: "#46A29F" }} >About</Paper>
+                            </Grid>
+                            <Grid item xs={12}>
+                                <Typography paragraph>
+                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
+                                    ut labore et dolore magna aliqua. Rhoncus dolor purus non enim praesent elementum
+                                    facilisis leo vel. Risus at ultrices mi tempus imperdiet. Semper risus in hendrerit
+                                    gravida rutrum quisque non tellus. Convallis convallis tellus id interdum velit laoreet id
+                                    donec ultrices. Odio morbi quis commodo odio aenean sed adipiscing. Amet nisl suscipit
+                                    adipiscing bibendum est ultricies integer quis. Cursus euismod quis viverra nibh cras.
+                                    Metus vulputate eu scelerisque felis imperdiet proin fermentum leo. Mauris commodo quis
+                                    imperdiet massa tincidunt. Cras tincidunt lobortis feugiat vivamus at augue. At augue eget
+                                    arcu dictum varius duis at consectetur lorem. Velit sed ullamcorper morbi tincidunt. Lorem
+                                    donec massa sapien faucibus et molestie ac.
+                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
+                                    ut labore et dolore magna aliqua. Rhoncus dolor purus non enim praesent elementum
+                                    facilisis leo vel. Risus at ultrices mi tempus imperdiet. Semper risus in hendrerit
+                                    gravida rutrum quisque non tellus. Convallis convallis tellus id interdum velit laoreet id
+                                    donec ultrices. Odio morbi quis commodo odio aenean sed adipiscing. Amet nisl suscipit
+                                    adipiscing bibendum est ultricies integer quis. Cursus euismod quis viverra nibh cras.
+                                    Metus vulputate eu scelerisque felis imperdiet proin fermentum leo. Mauris commodo quis
+                                    imperdiet massa tincidunt. Cras tincidunt lobortis feugiat vivamus at augue. At augue eget
+                                    arcu dictum varius duis at consectetur lorem. Velit sed ullamcorper morbi tincidunt. Lorem
+                                    donec massa sapien faucibus et molestie ac.
+                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
+                                    ut labore et dolore magna aliqua. Rhoncus dolor purus non enim praesent elementum
+                                    facilisis leo vel. Risus at ultrices mi tempus imperdiet. Semper risus in hendrerit
+                                    gravida rutrum quisque non tellus. Convallis convallis tellus id interdum velit laoreet id
+                                    donec ultrices. Odio morbi quis commodo odio aenean sed adipiscing. Amet nisl suscipit
+                                    adipiscing bibendum est ultricies integer quis. Cursus euismod quis viverra nibh cras.
+                                    Metus vulputate eu scelerisque felis imperdiet proin fermentum leo. Mauris commodo quis
+                                    imperdiet massa tincidunt. Cras tincidunt lobortis feugiat vivamus at augue. At augue eget
+                                    arcu dictum varius duis at consectetur lorem. Velit sed ullamcorper morbi tincidunt. Lorem
+                                    donec massa sapien faucibus et molestie ac.
+                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
+                                    ut labore et dolore magna aliqua. Rhoncus dolor purus non enim praesent elementum
+                                    facilisis leo vel. Risus at ultrices mi tempus imperdiet. Semper risus in hendrerit
+                                    gravida rutrum quisque non tellus. Convallis convallis tellus id interdum velit laoreet id
+                                    donec ultrices. Odio morbi quis commodo odio aenean sed adipiscing. Amet nisl suscipit
+                                    adipiscing bibendum est ultricies integer quis. Cursus euismod quis viverra nibh cras.
+                                    Metus vulputate eu scelerisque felis imperdiet proin fermentum leo. Mauris commodo quis
+                                    imperdiet massa tincidunt. Cras tincidunt lobortis feugiat vivamus at augue. At augue eget
+                                    arcu dictum varius duis at consectetur lorem. Velit sed ullamcorper morbi tincidunt. Lorem
+                                    donec massa sapien faucibus et molestie ac.
+                                
+                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
+                                    ut labore et dolore magna aliqua. Rhoncus dolor purus non enim praesent elementum
+                                    facilisis leo vel. Risus at ultrices mi tempus imperdiet. Semper risus in hendrerit
+                                    gravida rutrum quisque non tellus. Convallis convallis tellus id interdum velit laoreet id
+                                    donec ultrices. Odio morbi quis commodo odio aenean sed adipiscing. Amet nisl suscipit
+                                    adipiscing bibendum est ultricies integer quis. Cursus euismod quis viverra nibh cras.
+                                    Metus vulputate eu scelerisque felis imperdiet proin fermentum leo. Mauris commodo quis
+                                    imperdiet massa tincidunt. Cras tincidunt lobortis feugiat vivamus at augue. At augue eget
+                                    arcu dictum varius duis at consectetur lorem. Velit sed ullamcorper morbi tincidunt. Lorem
+                                    donec massa sapien faucibus et molestie ac.
+                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
+                                    ut labore et dolore magna aliqua. Rhoncus dolor purus non enim praesent elementum
+                                    facilisis leo vel. Risus at ultrices mi tempus imperdiet. Semper risus in hendrerit
+                                    gravida rutrum quisque non tellus. Convallis convallis tellus id interdum velit laoreet id
+                                    donec ultrices. Odio morbi quis commodo odio aenean sed adipiscing. Amet nisl suscipit
+                                    adipiscing bibendum est ultricies integer quis. Cursus euismod quis viverra nibh cras.
+                                    Metus vulputate eu scelerisque felis imperdiet proin fermentum leo. Mauris commodo quis
+                                    imperdiet massa tincidunt. Cras tincidunt lobortis feugiat vivamus at augue. At augue eget
+                                    arcu dictum varius duis at consectetur lorem. Velit sed ullamcorper morbi tincidunt. Lorem
+                                    donec massa sapien faucibus et molestie ac.
+                            </Typography>
+                            </Grid>
+                            
+                        </Grid>
+
+                    </div>
+
+                </div>
+            </React.Fragment>
+        );
+    };
+}
+export default Dashboard;
